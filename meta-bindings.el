8b165abdce8da10dfe64c09015192c384e84fc07
Ilya Silvestrov <tellary@gmail.com>
2014-12-12 17:24:49 -0800

    Added empty lines b/w sections

--- meta-bindings.el
+++ meta-bindings.el
@@ -12,20 +12,24 @@
 (global-set-key "\M-e" 'move-end-of-line)
 (global-set-key "\M-\S-a" 'beginning-of-buffer)
 (global-set-key "\M-\S-e" 'end-of-buffer)
+
 ;; ## Selection and copy, cut and paste
 (global-set-key "\M-y" 'yank)
 (global-set-key "\M-\S-y" 'yank-pop)
+
 ; M-w is bound by default to 'kill-ring-save
 (global-set-key "\M-\S-w" 'kill-region)
 (global-set-key "\M- " 'set-mark-command)
 (global-set-key "\M-d" 'backward-kill-word)
 (global-set-key "\M-\S-d" 'backward-delete-char-untabify)
+
 ;; ## Search
 (global-set-key "\M-s" 'isearch-forward)
 (global-set-key "\M-r" 'isearch-backward)
 (define-key isearch-mode-map "\M-s" 'isearch-repeat-forward)
 (define-key isearch-mode-map "\M-r" 'isearch-repeat-backward)
 (define-key isearch-mode-map "\M-g" 'isearch-abort)
+
 ;; ## Windows
 (defun select-next-window ()
   (interactive)
@@ -38,9 +42,10 @@
 (global-set-key "\M-}" 'select-next-window)
 (define-key markdown-mode-map "\M-{" 'select-previous-window)
 (define-key markdown-mode-map "\M-}" 'select-next-window)
+
 ;; ## Other
 (global-set-key "\M--" 'undo)
 (global-set-key "\M-\S-s" 'save-buffer)
 (global-set-key "\M-k" 'kill-line)
 (global-set-key "\M-g" 'keyboard-quit)
-
+(global-set-key "\M-u" 'universal-argument)
